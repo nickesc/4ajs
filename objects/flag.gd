1a39aa9f7a57b453aba168361fbc1a184c362aa5
N. Escobar <nick@nescobar.media>
2025-04-05 01:39:56 -0400

    update transition code

--- objects/flag.gd
+++ objects/flag.gd
@@ -1,8 +1,10 @@
 extends Node3D
 
-@export var transition_to: PackedScene
+@export_file('*.tscn') var transition_to: String
 @export var player: Node3D
 
 func _on_body_entered(body):
     if (body == player):
-        get_tree().change_scene_to_packed(transition_to)
+        print("Flag reached. Scene transition incoming.")
+        var next_scene = load(transition_to)
+        get_tree().change_scene_to_packed(next_scene)
