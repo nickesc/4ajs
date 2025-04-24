3bf87d21879ad42de5e80daf48f0ecc141221798
N. Escobar <nick@nescobar.media>
2025-04-24 05:03:10 -0400

    update main menu to use 3d text

--- scripts/menu_platter.gd
+++ scripts/menu_platter.gd
@@ -2,5 +2,8 @@ extends Node3D
 
 @export_range(.1, 1) var rotation_speed: float = 1
 
+@onready var title: MeshInstance3D = $Title
+
 func _process(delta):
     rotate_y(rad_to_deg(rotation_speed) * delta/10)
+    title.rotate_y(rad_to_deg(-rotation_speed) * delta/10)
