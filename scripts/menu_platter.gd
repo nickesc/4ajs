47801e2e496af7be6de5dd566750822100fded3f
N. Escobar <nick@nescobar.media>
2025-04-26 15:18:46 -0400

    add on toggle for menu spinning

--- scripts/menu_platter.gd
+++ scripts/menu_platter.gd
@@ -1,9 +1,11 @@
 extends Node3D
 
 @export_range(.1, 1) var rotation_speed: float = 1
+@export var on: bool = true
 
 @onready var title: MeshInstance3D = $Title
 
 func _process(delta):
-    rotate_y(rad_to_deg(rotation_speed) * delta/10)
-    title.rotate_y(rad_to_deg(-rotation_speed) * delta/10)
+    if on:
+        rotate_y(rad_to_deg(rotation_speed) * delta/10)
+        title.rotate_y(rad_to_deg(-rotation_speed) * delta/10)
